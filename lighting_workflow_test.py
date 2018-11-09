51d33335472522ef246b8f12084e3d45412017cd
chris winters <winteralfs@aol.com>
2018-11-09 16:18:48 -0500

    Add files via upload

---
+++ lighting_workflow_test.py
@@ -0,0 +1,9 @@
+__LABEL_NAME__ = "TESTING | PLEASE IGNORE"
+__SUBMENU__ = "Lighting"
+__DESCRIPTION__ = "this is a workflow test"
+
+def body():
+    print "test script lighting beta"
+
+def main():
+  body()
